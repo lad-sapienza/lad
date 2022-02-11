8c0cd5f2a3d22ddd983262568b7aa35a6411d175
Julian Bogdani <jbogdani@gmail.com>
2022-02-11 07:40:05 +0100

    Sistemato Team

--- src/components/team.js
+++ src/components/team.js
@@ -9,103 +9,77 @@ import { SiAcademia, SiResearchgate } from "react-icons/si";
 import { GiIceIris } from "react-icons/gi";
 
 const Team = () => {
+
+  const teamData = [
+    {
+      "name": "Julian Bogdani",
+      "image": "images/jbogdani.jpg",
+      "position": "Responsabile del laboratorio",
+      "affiliation": "Ricercatore di Metodologie della Ricerca Archeologica, Dip. SARAS",
+      "uni": "https://www.lettere.uniroma1.it/users/julian-bogdani",
+      "github": "https://github.com/jbogdani",
+      "academia": "https://uniroma1.academia.edu/JulianBogdani",
+      "iris": "https://iris.uniroma1.it/browse?type=author&authority=rp62440&authority_lang=en",
+      "researchgate": "https://www.researchgate.net/profile/Julian-Bogdani",
+      "twitter": "https://twitter.com/JulianBogdani"
+    },
+    {
+      "name": "Domizia D'Erasmo",
+      "image": "images/dderasmo.jpg",
+      "position": "Tutor",
+      "affiliation": "Assegnista di ricerca di Metodologie della Ricerca Archeologica, Dip. SARAS",
+      "github": "https://github.com/ddomizia",
+      "academia": "https://uniroma1.academia.edu/domiziaderasmo",
+      "iris": "https://iris.uniroma1.it/simple-search?query=Domizia+D%27Erasmo&needescape=1",
+      "researchgate": "https://www.researchgate.net/profile/Domizia_Derasmo",
+      "twitter": "https://twitter.com/DomiziaDErasmo"
+    },
+    {
+      "name": "Paolo Rosati",
+      "image": "images/prosati.jpg",
+      "position": "Tutor",
+      "affiliation": "Assegnista di ricerca del progetto PAThs, Dip. SARAS",
+      "academia": "https://uniroma1.academia.edu/RosatiPaolo",
+      "researchgate": "https://www.researchgate.net/profile/Paolo-Rosati-2"
+    },
+  ];
   return (
     <Wrapper>
       <Container>
         <h2 className="border-bottom">Chi siamo</h2>
         <CardGroup>
-          {/* Julian */}
-          <Card>
-            <Card.Img variant="top" src="images/jbogdani.jpg" />
-            <Card.Body>
-              <Card.Title>Julian Bogdani</Card.Title>
-              <Card.Text className="position">
-                Responsabile del laboratorio
-              </Card.Text>
-              <Card.Text className="dipartimento">
-                Ricercatore di Metodologie della Ricerca Archeologica, Dip.
-                SARAS
-              </Card.Text>
-              <Card.Text className="icon">
-                <a href="https://www.lettere.uniroma1.it/users/julian-bogdani">
-                  <FaUniversity />
-                </a>
-                <a href="https://github.com/jbogdani">
-                  <FaGithub />
-                </a>
-                <a href="https://uniroma1.academia.edu/JulianBogdani">
-                  <SiAcademia />
-                </a>
-                <br />
-                <a href="https://iris.uniroma1.it/browse?type=author&authority=rp62440&authority_lang=en">
-                  <GiIceIris />
-                </a>
-                <a href="https://www.researchgate.net/profile/Julian-Bogdani">
-                  <SiResearchgate />
-                </a>
-                <a href="https://twitter.com/JulianBogdani">
-                  <FaTwitter />
-                </a>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-
-          {/* Domizia */}
-          <Card>
-            <Card.Img variant="top" src="images/dderasmo.jpg" />
-            <Card.Body>
-              <Card.Title>Domizia D'Erasmo</Card.Title>
-              <Card.Text className="position">Tutor</Card.Text>
-                <Card.Text className="dipartimento">
-                  Assegnista di ricerca di Metodologie della Ricerca
-                  Archeologica, Dip. SARAS
-                </Card.Text>
+          {teamData.map( m => {
+            return (
+            <Card>
+              <Card.Img variant="top" src={ m.image } />
+              <Card.Body>
+                <Card.Title>{ m.name }</Card.Title>
+                <Card.Text className="position">{ m.position }</Card.Text>
+                <Card.Text className="dipartimento">{ m.affiliation }</Card.Text>
                 <Card.Text className="icon">
-                  <a href="https://github.com/ddomizia">
+                  { m.uni && <a href={ m.uni } title="Sito istituzionale">
+                    <FaUniversity />
+                  </a>}
+                  { m.github && <a href={ m.github } title="Github">
                     <FaGithub />
-                  </a>
-                  <a href="https://uniroma1.academia.edu/domiziaderasmo">
+                  </a>}
+                  { m.academia && <a href={ m.academia } title="Academia.edu">
                     <SiAcademia />
-                  </a>
-                  <a
-                    href="https://iris.uniroma1.it/simple-search?query=Domizia+D%27Erasmo&needescape=1"
-                  >
+                  </a>}
+                  { m.iris && <a href={ m.iris } title="Catalogo IRIS">
                     <GiIceIris />
-                  </a>
-                  <a
-                    href="https://www.researchgate.net/profile/Domizia_Derasmo"
-                  >
+                  </a>}
+                  { m.researchgate && <a href={ m.researchgate } title="Research Gate">
                     <SiResearchgate />
-                  </a>
-                  <a href="https://twitter.com/DomiziaDErasmo">
+                  </a>}
+                  { m.twitter && <a href={ m.twitter } title="Twitter">
                     <FaTwitter />
-                  </a>
+                  </a>}
                 </Card.Text>
-            </Card.Body>
-          </Card>
-
-          {/* Paolo */}
-          <Card>
-            <Card.Img variant="top" src="images/prosati.jpg" />
-            <Card.Body>
-              <Card.Title>Paolo Rosati</Card.Title>
-              <Card.Text className="position">Tutor</Card.Text>
-              <Card.Text className="dipartimento">
-                Assegnista di ricerca del progetto <em>PAThs</em>, Dip. SARAS
-              </Card.Text>
-              <Card.Text className="icon">
-                {/* <a href="#">
-                  <FaGithub />
-                </a> */}
-                <a href="https://uniroma1.academia.edu/RosatiPaolo">
-                  <SiAcademia />
-                </a>
-                <a href="https://www.researchgate.net/profile/Paolo-Rosati-2">
-                  <SiResearchgate />
-                </a>
-              </Card.Text>
-            </Card.Body>
-          </Card>
+              </Card.Body>
+            </Card>
+          )
+          })}
         </CardGroup>
       </Container>
     </Wrapper>
@@ -115,26 +89,22 @@ const Team = () => {
 const Wrapper = styled.section`
   img {
     filter: grayscale(100%);
-  }
-  img:hover {
-    filter: none;
+    &:hover {
+      filter: none;
+    }
   }
   svg {
-    margin-right: 1rem;
-  }
-  p .card-text {
-    font-family: "Raleway", sans-serif;
-    font-weight: 300;
-    font-size: 1rem;
+    margin-right: .5rem;
+    margin-left: .5rem;
   }
   .card {
-    margin-right: 0.4rem;
+    padding: .5rem;
     border: none;
   }
   .card-title {
     font-family: "Cormorant Garamond", serif !important;
-    font-weight: 800;
-    font-size: 1.5rem;
+    font-weight: 900;
+    font-size: 1.4rem;
     text-align: center;
   }
   .card-text.dipartimento {
@@ -147,9 +117,9 @@ const Wrapper = styled.section`
   .card-text.position {
     font-family: "Cormorant Garamond", serif !important;
     font-weight: 800;
-    font-style: italic;
     font-size: 1rem !important;
     text-align: center;
+    line-height: 1.2rem;
   }
 `;
 
