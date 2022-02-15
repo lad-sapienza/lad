974c10d55722bca8bfeb98e924fde009baa04478
Julian Bogdani <jbogdani@gmail.com>
2022-02-15 10:43:41 +0100

    Aggiunta info provacy footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ import LAD from "../../static/logos/lad-blue.png";
 const Footer = () => {
     return (
       <Wrapper>
-        <footer>
+        <footer className="mt-5 border-top">
           <Container className="pt-5 my-5">
             <Row>
               <Col sm={3} className="text-center d-flex align-items-center">
@@ -29,11 +29,17 @@ const Footer = () => {
               </Col>
               <Col sm={4} className="d-flex align-items-center">
                   <h5 className="text-center">
-                    <a href="https://saras.uniroma1.it/" alt="SARAS" className="green">
+                    <a href="https://saras.uniroma1.it/" alt="SARAS" className="green fs-4">
                       Dipartimento di Storia Antropologia Religioni Arte
                       Spettacolo.<br /> Sapienza Università di Roma
                     </a>
                   </h5>
+              </Col>
+              <Col sm={5} className="text-center">
+                <h5>Nota sulla <em>privacy</em></h5>
+                <p>
+                  Questo sito <strong>non</strong> traccia l'attività dei suoi lettori e <strong>non</strong> raccoglie dati personali sulla navigazione, cronologia, preferenze, indirizzo IP, geolocalizzazione o altre informazioni personali, aggregate o meno. Il LAD ha scelto di non raccogliere statistiche sul proprio utilizzo per difendere il diritto alla riservatezza dei propri utenti.
+                </p>
               </Col>
             </Row>
           </Container>
@@ -52,12 +58,6 @@ const Wrapper = styled.section`
   }
   img {
     max-width: 180px !important;
-  }
-  footer {
-    margin-top: 5rem;
-    border-top: solid;
-    border-width: 0.1rem;
-    border-color: rgb(145, 145, 145);
   }
   a.green {
     color: rgba(21, 71, 82, 0.8) !important;
