bcf6ef513c74287643811c3f5d9cab6f5fa4aaea
ddomizia <domizia.derasmo@gmail.com>
2022-04-05 11:49:30 +0200

    Aggunto plugin `gatsby-plugin-sitemap`

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sass`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-next-seo`,
       options: {
