ca49d59fb738f37a4fcf11e0399d0b9dcb97c133
Julian Bogdani <jbogdani@gmail.com>
2022-05-12 09:33:41 +0200

    Full support for optional pinned field

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,7 +19,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
   const result = await graphql(`
     query {
-      allMarkdownRemark {
+      allMarkdownRemark (filter: { frontmatter: {title: {ne: "Dummy content"}}}){
         edges {
           node {
             fields {
---
+++ posts/dummy.md
@@ -0,0 +1,7 @@
+---
+title: Dummy content
+img: ./notizie/2021-03-08-convegno-landscape-3/lab-lad.png
+date: 2022-02-04
+sommario: Dummy summary
+pinned: true
+---
