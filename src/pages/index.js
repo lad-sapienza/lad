9596ac6fef58878345362062feb1928e0981366e
Julian Bogdani <jbogdani@gmail.com>
2023-09-06 11:09:29 +0200

    Aggiunto bottone per tutte le notizie

--- src/pages/index.js
+++ src/pages/index.js
@@ -215,6 +215,15 @@ const Index = ({ data }) => {
                     readMore={node.fields.slug}
                   />
                 ))}
+                <div class="d-grid gap-2 col-12 mx-auto my-4">
+                  <Link
+                    to="/Notizie/"
+                    className="btn btn-outline-primary"
+                    title="Vai al Blog del LAD"
+                  >
+                    Vai all'elenco completo delle notizie
+                  </Link>
+                </div>
               </Container>
 
               <Container>
@@ -229,15 +238,16 @@ const Index = ({ data }) => {
                     readMore={node.fields.slug}
                   />
                 ))}
-              </Container>
-              <Container className="link-blog text-center">
-                <Link
-                  to="/blog/"
-                  className="btn btn-primary text-white"
-                  title="Vai al Blog del LAD"
-                >
-                  Vai all'elenco completo degli articoli della sezione Blog
-                </Link>
+
+                <div class="d-grid gap-2 col-12 mx-auto my-4">
+                  <Link
+                    to="/blog/"
+                    className="btn btn-outline-primary"
+                    title="Vai al Blog del LAD"
+                  >
+                    Vai all'elenco completo degli articoli della sezione Blog
+                  </Link>
+                </div>
               </Container>
             </Col>
           </Row>
