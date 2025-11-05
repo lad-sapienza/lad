9ba71ea159e591da560ec3892d6e46472251149c
Julian Bogdani <jbogdani@gmail.com>
2025-11-05 10:13:01 +0100

    feat: add gatsby-remark-copy-linked-files plugin for improved file handling

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,13 @@ module.exports = {
               noInlineHighlight: false,
             },
           },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+              destinationDir: `static`,
+              ignoreFileExtensions: [],
+            },
+          },
         ],
       },
     },
