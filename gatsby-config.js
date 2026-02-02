8c8020b1730138940168f3ba731471c9c9de4004
Julian Bogdani <jbogdani@gmail.com>
2026-02-02 11:07:15 +0100

    feat: add gatsby-remark-images plugin and update image extensions to .jpg

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,16 @@ module.exports = {
         gatsbyRemarkPlugins: [
           `gatsby-remark-autolink-headers`,
           {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+              quality: 80,
+              withWebp: true,
+              linkImagesToOriginal: false,
+              backgroundColor: 'transparent',
+            },
+          },
+          {
             resolve: `gatsby-remark-prismjs`,
             options: {
               classPrefix: "language-",
